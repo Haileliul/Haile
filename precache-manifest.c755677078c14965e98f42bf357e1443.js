7abd57cd21d9cbb6865aa4cc72b2333c10fc02b9
Haileliul <hailetechlover@gmail.com>
2022-08-28 13:04:31 +0300

    Updates

---
+++ precache-manifest.c755677078c14965e98f42bf357e1443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6b3bd68552e212aeb98",
+    "url": "/Haile/static/css/main.e44683d8.chunk.css"
+  },
+  {
+    "revision": "f6b3bd68552e212aeb98",
+    "url": "/Haile/static/js/main.f6b3bd68.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Haile/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/Haile/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "4bdc7cc700b6bb15102f",
+    "url": "/Haile/static/js/runtime~main.4bdc7cc7.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Haile/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "86de31b94c18e4cccb579faf751d43d0",
+    "url": "/Haile/index.html"
+  }
+];
